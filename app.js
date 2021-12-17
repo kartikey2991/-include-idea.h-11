c054fff3a8feb564a0957bc0cb00f9c4d6d62a3c
Kartikey Gupta <79379134+kartikey2991@users.noreply.github.com>
2021-12-17 12:55:13 +0530

    okay

--- app.js
+++ app.js
@@ -1,17 +1,64 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+// const { check, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/gfg");
+const db = mongoose.connection;
+
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback) {
+    console.log("conncection succeeded");
+})
+
+// const Register = require("./models/registers");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get("/", function(req, res) {
-    res.render("index");
+    res.set({
+        'Access-control-Allow-Origin': '*'
+    });
+    return res.redirect("index");
 })
 
 app.get("/register", function(req, res) {
-    res.render("register");
+    var name = req.body.name;
+    var email = req.body.email;
+    var pass = req.body.password;
+    var phone = req.body.phone;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "password": pass,
+        "phone": phone,
+    }
+    db.collection('details').insertOne(data, function(err, collection) {
+        if (err) throw err;
+        console.log("Record inserted successfully");
+    });
+
+    return res.redirect("register");
+})
+
+// app.post("/register", urlencodedParser, function(req, res) {
+//     res.json(req.body)
+// })
+
+app.post("/register", async(req, res) => {
+    try {
+        console.log()
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
 })
 
 app.listen(3000, function() {
---
+++ models/registers.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        //unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        //unique: true
+    },
+})
+
+const Register = new mongoose.model("Register", employeeSchema);
+
+module.exports = Register;
